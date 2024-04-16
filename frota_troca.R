0f4231fb6622f6da5e31303f61f08f7013a44a68
Thiago Faustino <83605855+thfaustino@users.noreply.github.com>
2024-04-16 16:21:00 -0300

    Update frota_troca.R
    Version 2.0
    Correções de BUG na Idade Média

--- frota_troca.R
+++ frota_troca.R
@@ -21,9 +21,9 @@ rm(frota_transformada)
 setwd(choose.dir())
 
 #PARAMETROS DE MÉDIA
-max<-0
-med<-0
-i<-as.numeric(format(Sys.Date(),"%Y"))+1
+max<-1
+med<-1
+i<-as.numeric(format(Sys.Date(),"%Y"))
 
 #CALCULO IDADE
 frota$ANO<-as.numeric(frota$ANO)
@@ -33,7 +33,7 @@ frota2<-frota
 
 for(max in 1:30){
   for(med in 1:20){
-    
+    if(med<=max){
     frota2<-frota
     compra_ano<-frota2 %>% dplyr::select(TP,QTDE)
     compra_ano[1:nrow(compra_ano),]<-NA
@@ -43,7 +43,7 @@ for(max in 1:30){
     if(max(frota2$IDADE)>=max){
       compra<-filter(frota2,IDADE>=max)
       compra$ANO<-i
-      compra$IDADE<-0
+      compra<-compra %>% dplyr::select(TP,ANO,QTDE) %>% dplyr::group_by(TP,ANO) %>% dplyr::summarise(QTDE=sum(QTDE),IDADE=0)
       compra_anocorrente<-compra %>% dplyr::select(TP,QTDE) %>% dplyr::group_by(TP) %>% dplyr::summarise(QTDE=sum(QTDE))
       compra_anocorrente$ANO_COMPRA<-i
       compra_ano<-rbind(compra_ano,compra_anocorrente)
@@ -51,7 +51,6 @@ for(max in 1:30){
       frota2<-rbind(frota2,compra)
       frota2<-frota2 %>% dplyr::select(TP,ANO,QTDE,IDADE) %>% dplyr::group_by(TP,ANO) %>% dplyr::summarise(QTDE=sum(QTDE))
       frota2$IDADE<-i-as.numeric(frota2$ANO)
-    }else if((sum(frota2$IDADE*frota2$QTDE)/sum(frota2$QTDE))>med){
       while(sum(frota2$IDADE*frota2$QTDE)/sum(frota2$QTDE)>med){
         compra<-filter(frota2,IDADE == max(frota2$IDADE))
         compra$ANO<-i
@@ -63,17 +62,27 @@ for(max in 1:30){
         frota2<-rbind(frota2,compra)
         frota2<-frota2 %>% dplyr::select(TP,ANO,QTDE,IDADE) %>% dplyr::group_by(TP,ANO) %>% dplyr::summarise(QTDE=sum(QTDE))
         frota2$IDADE<-i-as.numeric(frota2$ANO)
-        }
       }
+    }else if (sum(frota2$IDADE*frota2$QTDE)/sum(frota2$QTDE)>med){
+      while(sum(frota2$IDADE*frota2$QTDE)/sum(frota2$QTDE)>med){
+      compra<-filter(frota2,IDADE == max(frota2$IDADE))
+      compra$ANO<-i
+      compra$IDADE<-0
+      compra_anocorrente<-compra %>% dplyr::select(TP,QTDE) %>% dplyr::group_by(TP) %>% dplyr::summarise(QTDE=sum(QTDE))
+      compra_anocorrente$ANO_COMPRA<-i
+      compra_ano<-rbind(compra_ano,compra_anocorrente)
+      frota2<-filter(frota2,IDADE != max(frota2$IDADE))
+      frota2<-rbind(frota2,compra)
+      frota2<-frota2 %>% dplyr::select(TP,ANO,QTDE,IDADE) %>% dplyr::group_by(TP,ANO) %>% dplyr::summarise(QTDE=sum(QTDE))
+      frota2$IDADE<-i-as.numeric(frota2$ANO) 
+    }
+    }
     }
     compra_ano$MED<-med
     compra_ano$MAX<-max
     compra_ano<-filter(compra_ano,!is.na(compra_ano$TP))
     write.csv2(compra_ano,str_c('compra_ano_',med,'_',max,'.csv'),row.names=F)
+    print(str_c("Cenário ",max," ",med," terminado."))
+  }
   }
 }
-
-
-
-
-
