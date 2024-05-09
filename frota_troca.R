b94fda70291c78331f974cceb4312f960825eff2
Thiago Faustino <thfaustino@gmail.com>
2024-05-09 09:31:53 -0300

    Inserção de novo dataset com restrição orçamentária e o total de veículos vendidos por ano. O total em VEICULOS_VENDIDOS diz respeito a cada veículo Padron e Básico que pode ser vendido com os orçamentos finais de contrato (5 anos)

--- frota_troca.R
+++ frota_troca.R
@@ -7,7 +7,7 @@ library(lpSolve)
 library(stringr)
 
 #CARREGANDO ARQUIVOS
-frota<-read.xlsx(file.choose())
+frota<-read.xlsx('frota_atual.xlsx')
 frota_transformada <- pivot_longer(frota, cols = -Ano.de.Fabricação, names_to = "Ano", values_to = "Qtde")
 frota<-filter(frota_transformada,frota_transformada$Qtde!=0)
 colnames(frota)<-c("TP","ANO","QTDE")
@@ -17,19 +17,18 @@ frota$TP<-gsub(' 3','',frota$TP)
 frota$TP<-gsub(' 4','',frota$TP)
 frota$TP<-gsub(' 5','',frota$TP)
 rm(frota_transformada)
-
-setwd(choose.dir())
-
-#PARAMETROS DE MÉDIA
-max<-1
-med<-1
-i<-as.numeric(format(Sys.Date(),"%Y"))
+custo<-read.xlsx("custo_por_veiculo.xlsx")
+custo$`CUSTO.(jul/23)`<-NULL
 
 #CALCULO IDADE
+i<-as.numeric(format(Sys.Date(),"%Y"))
 frota$ANO<-as.numeric(frota$ANO)
 frota$IDADE<-i-frota$ANO
 frota2<-frota
 
+#PARAMETROS DE MÉDIA E MÁXIMA
+max<-1
+med<-1
 
 for(max in 1:30){
   for(med in 1:20){
@@ -85,4 +84,42 @@ for(max in 1:30){
     print(str_c("Cenário ",max," ",med," terminado."))
   }
   }
+}
+
+##PARAMETROS DE CUSTOS
+budget<-seq(10000000,2000000000,by=10000000)
+frota2<-frota
+colnames(custo)<-c("TP","CUSTO_UN")
+frota2<-merge(frota2,custo,by.x="TP",by.y="TP")
+frota2 <- frota2 %>% mutate(index = row_number())
+dados_duplicados <- frota2 %>% slice(rep(1:n(), QTDE)) %>% select(-QTDE) 
+rownames(dados_duplicados) <- NULL
+frota2<-arrange(dados_duplicados,TP,ANO)
+rm(dados_duplicados)
+frota2$index<-NULL
+frota2$ID<-c(1:nrow(frota2))
+
+j<-2024
+i<-0
+custo<-arrange(custo,desc(CUSTO_UN))
+custo$CUSTO_UN<-0.025*custo$CUSTO_UN
+
+setwd('VEICULOS_VENDIDOS')
+
+for(i in 1:length(budget)){
+  frota3<-rbind(filter(frota2,TP=="PADRON"),filter(frota2,TP=="BASICO"))
+  for(j in 2024:2029){
+    custo_aquisicao<-0
+    while(custo_aquisicao<=budget[i]){
+      venda<-frota3[1,]
+      frota3[1,]<-NA
+      frota3<-filter(frota3,!is.na(TP))
+      venda$ANO<-j
+      frota3<-rbind(frota3,venda)
+      custo_aquisicao<-custo_aquisicao+venda$CUSTO_UN[1]
+    }
+  }
+  veiculos_vendidos<-filter(frota3,ANO %in% c(2024:2054))
+  veiculos_vendidos$budget<-budget[i]*5
+  write.csv(veiculos_vendidos,str_c('veiculos_comprados_budget_',budget[i],'.csv'),row.names = F)
 }
